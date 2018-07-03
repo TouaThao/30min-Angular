6800581cb9c9e70e022a9344a220795378ca685d
Toua Thao <touathao09@gmail.com>
2018-07-03 16:44:46 -0500

    Did more than what was ask of me ...

---
+++ server/public/script/client.js
@@ -0,0 +1,18 @@
+let foodApp = angular.module('foodApp', []);
+foodApp.controller('FoodController', [function(){
+    let self = this
+    self.test = 'This is Test'
+    self.people = ['Me','Myself','I']
+    self.inConsole = ''
+    self.showArrayObject = [{ Food:'pizza', cost:'10.99'},{Food:'hamburger', cost:'15'}]
+    self.showMe = false;
+    self.invisible = function(){
+        self.showMe = !self.showMe
+    }
+    self.press = function(){
+        console.log(self.first)
+    self.secondPress = function(){
+        console.log(self.second)
+    }
+    }
+}])
